8be4628e2c608cac2cbae52f013630d5b36ba3c5
-1 <sa.akash0129@gmail.com>
2020-03-14 15:31:15 +0600

    payment initial activity added

--- app/src/main/java/ninja/saad/bkashdemo/features/bkash/BkashPaymentActivity.kt
+++ app/src/main/java/ninja/saad/bkashdemo/features/bkash/BkashPaymentActivity.kt
@@ -1,9 +1,10 @@
- package ninja.saad.bkashdemo
+ package ninja.saad.bkashdemo.features.bkash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ninja.saad.bkashdemo.R
 
-class MainActivity : AppCompatActivity() {
+ class BkashPaymentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
